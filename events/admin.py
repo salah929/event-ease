1ac6838b7cd2bf0c09623d9f3c335688f4f387f5
salah929 <salah.sy@msn.com>
2025-06-19 04:42:52 +0200

    Register the models in admin site

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Event, EventRegistration, ContactMessage
 
-# Register your models here.
+admin.site.register(Event)
+admin.site.register(EventRegistration)
+admin.site.register(ContactMessage)
