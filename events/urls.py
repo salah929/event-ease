513b18039de65af6d2b6cbafcce7e198fef7a08a
salah929 <salah.sy@msn.com>
2025-06-20 02:25:55 +0200

    Add PastEventList view and past_event.html template

--- events/urls.py
+++ events/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.UpcomingEventList.as_view(), name='home'),
+    path('past-events/', views.PastEventList.as_view(), name='past_events'),
 ]
--- events/views.py
+++ events/views.py
@@ -16,3 +16,17 @@ class UpcomingEventList(generic.ListView):
             .filter(status=1, date__gte=date.today())
             .order_by('date', 'time')
         )
+
+
+class PastEventList(generic.ListView):
+    model = Event
+    template_name = 'events/past_events.html'
+    context_object_name = 'past_events'
+    paginate_by = 6
+
+    def get_queryset(self):
+        return (
+            Event.objects
+            .filter(status=1, date__lt=date.today())
+            .order_by('-date', '-time')
+        )
