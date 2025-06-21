3c6152fe5bf98e28c1f46e876dad0734d7941d58
salah929 <salah.sy@msn.com>
2025-06-21 03:35:09 +0200

    Add basic login, logout and signup templates

--- eventease/urls.py
+++ eventease/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('events.urls')),
+    path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
 ]
