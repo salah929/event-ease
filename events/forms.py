6329d30605a24e57ccf296e37c5ed9d898606940
salah929 <salah.sy@msn.com>
2025-06-26 01:25:21 +0200

    Add image upload to events with Cloudinary integration and size validation

--- events/forms.py
+++ events/forms.py
@@ -1,11 +1,16 @@
 from django import forms
 from .models import Event, EventRegistration, ContactMessage
+from django.forms import ClearableFileInput
+from django.core.exceptions import ValidationError
 
 
 class EventForm(forms.ModelForm):
     class Meta:
         model = Event
-        fields = ['title', 'description', 'date', 'time', 'location']
+        fields = [
+            'title', 'description', 'date', 'time',
+            'location', 'featured_image'
+        ]
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'description': forms.Textarea(attrs={
@@ -18,7 +23,21 @@ class EventForm(forms.ModelForm):
                 'type': 'time', 'class': 'form-control'
             }),
             'location': forms.TextInput(attrs={'class': 'form-control'}),
+            'featured_image': ClearableFileInput(attrs={
+                'class': 'form-control',
+                'accept': 'image/*',
+            }),
         }
+
+    def clean_featured_image(self):
+        image = self.cleaned_data.get('featured_image')
+        max_size_mb = 3
+
+        if image and image.size > max_size_mb * 1024 * 1024:
+            raise ValidationError(
+                f"The image file is too large (>{max_size_mb}MB)."
+            )
+        return image
 
 
 class EventRegistrationForm(forms.ModelForm):
