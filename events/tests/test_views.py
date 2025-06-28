ab3ad349855019fdb126ee020b38753c78e47023
salah929 <salah.sy@msn.com>
2025-06-28 04:01:20 +0200

    Add one missed test (test_post_invalid_contact_form_shows_errors)

--- events/tests/test_views.py
+++ events/tests/test_views.py
@@ -300,3 +300,20 @@ class TestContactView(TestCase):
         self.assertEqual(msg.name, 'Mark Selby')
         self.assertEqual(msg.email, 'mark@example.com')
         self.assertEqual(msg.message, 'I would like to know more.')
+
+    def test_post_invalid_contact_form_shows_errors(self):
+        form_data = {
+            'name': '',
+            'email': 'not-an-email',
+            'message': ''
+        }
+        response = self.client.post(reverse('contactus'), form_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'events/contact.html')
+        self.assertFormError(
+            response, 'form', 'name', 'This field is required.'
+        )
+        self.assertFormError(
+            response, 'form', 'message', 'This field is required.'
+        )
+        self.assertEqual(ContactMessage.objects.count(), 0)
